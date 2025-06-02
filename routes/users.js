e406c44c97fc84e02cdc28a16b375d8a85503697
GolanKIBRI4 <gk599625@gmail.com>
2025-06-02 14:59:40 +0600

    Complete User Autkentication

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const plm=require('passport-local-mongoose');
+
+mongoose.connect('mongodb+srv://golamkibria:1234@cluster0.lzxkxdz.mongodb.net/authcheck?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  secret: String
+});
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User', userSchema);
